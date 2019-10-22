a2273ab50552c95633580eb531d82e65636397a4
zhengp0 <zhengp@uw.edu>
2019-10-22 12:30:07 -0700

    update: add inner and outer optimization solver.

--- src/limetr/optim.py
+++ src/limetr/optim.py
@@ -5,6 +5,8 @@
     Optimization module for limetr object.
 """
 import numpy as np
+from scipy.optimize import bisect
+import ipopt
 from . import linalg
 from . import utils
 
@@ -23,8 +25,6 @@ class OptimizationSolver:
         self.z = z
         self.s = s
         self.estimate_s = self.s is None
-        if self.estimate_s:
-            self.s = np.repeat(np.std(self.y), self.y.size)
 
         self.bl = bl
         self.bu = bu
@@ -46,6 +46,12 @@ class OptimizationSolver:
         if self.use_c:
             self.constraints = self.cf
             self.jacobian = self.cf.jac
+            self.k_c = self.cf.shape[0]
+        else:
+            self.k_c = 0
+            self.cl = []
+            self.cu = []
+
 
         self.pf = pf
         self.pm = pm
@@ -53,6 +59,13 @@ class OptimizationSolver:
         self.use_p = (self.pf is not None and
                       self.pm is not None and
                       self.pw is not None)
+
+        if self.use_p:
+            self.k_p = self.pf.shape[0]
+        else:
+            self.k_p = 0
+            self.pm = []
+            self.pw = []
 
         # extract the dimension
         self.k_beta = self.f.shape[1]
@@ -108,9 +121,103 @@ class OptimizationSolver:
             g += self.qw*(x - self.qm)
 
         if self.use_p:
-            g += self.pf.jac.T.dot(self.pw*(self.pf(x) - self.pm))
+            g += self.pf.jac(x).T.dot(self.pw*(self.pf(x) - self.pm))
 
         return g
+
+    def initialize_inner_vars(self):
+        # initialize gamma
+        gamma_init = np.repeat(0.1, self.k_gamma)
+        # initialize beta
+        beta_ones = np.ones(self.k_beta)
+        v = self.compute_v(gamma_init)
+        jf = self.f.jac(beta_ones)
+
+        mat = jf.T.dot(v.inv_dot(jf))/self.h
+        rhs = jf.T.dot(v.inv_dot(self.y))/self.h
+
+        if self.use_q:
+            mat += np.diag(self.qw)
+            rhs += self.qw*self.qm
+
+        if self.use_p:
+            jpf = self.pf.jac(beta_ones)
+            mat += (jpf.T*self.pw).dot(jpf)
+            rhs += (jpf.T*self.pw).dot(self.pm)
+
+        beta_init = np.linalg.pinv(mat).dot(rhs)
+
+        return np.hstack((beta_init, gamma_init))
+
+    def initialize_outer_vars(self, w_init=None, s_init=None):
+        if w_init is not None:
+            assert np.abs(w_init.sum() - self.h) < 1e-10
+            assert np.all(w_init >= 0.0) and np.all(w_init <= 1.0)
+            self.w = w_init
+        if self.estimate_s:
+            if s_init is not None:
+                assert np.all(s_init > 0.0)
+                self.s = s_init
+            else:
+                self.s = np.std(self.y)
+
+    def optimize_inner_vars(self, x, inner_solver_options):
+        inner_opt_problem = ipopt.problem(
+            n=self.k,
+            m=self.k_c,
+            problem_obj=self,
+            lb=self.bl,
+            ub=self.bu,
+            cl=self.cl,
+            cu=self.cu
+        )
+
+        # add options
+        for option_name in inner_solver_options.keys():
+            inner_opt_problem.addOption(option_name,
+                                        inner_solver_options[option_name])
+
+        inner_soln, inner_info = inner_opt_problem.solve(x)
+
+        return inner_soln
+
+    def optimize_outer_vars(self, inner_soln, outer_solver_options):
+        # update trimming weights
+        beta_soln, gamma_soln = self.unpack_x(inner_soln)
+        r = self.compute_r(beta_soln)
+        v = self.compute_v(gamma_soln)
+        g_w = r*v.inv_dot(r)
+        g_w_max = np.linalg.norm(g_w, np.inf)
+        if g_w_max != 0:
+            g_w /= g_w_max
+
+        self.w = proj_capped_simplex(
+            self.w - outer_solver_options["step_size"]*g_w,
+            self.h
+        )
+
+        # update observation standard deviation
+        if self.estimate_s:
+            u = self.compute_u(beta_soln, gamma_soln)
+            r = self.y - self.f(beta_soln) -\
+                np.sum(self.z*np.repeat(u, self.n, axis=0), axis=1)
+            self.s = np.std(r)
+
+    def compute_u(self, beta, gamma):
+        r = self.compute_r(beta)
+        inv_s2_z = self.z/(self.s**2).reshape(self.N, 1)
+        inv_gamma = 1.0/gamma
+
+        r_split = np.split(r, self.idx_split[1:])
+        z_split = np.split(self.z, self.idx_split[1:], axis=0)
+        inv_s2_z_split = np.split(inv_s2_z, self.idx_split, axis=0)
+
+        u = [np.linalg.solve(inv_s2_z_split[i].T.dot(z_split[i]) +
+                             np.diag(inv_gamma),
+                             inv_s2_z_split[i].T.dot(r_split[i]))
+             for i in range(self.m)]
+
+        return np.vstack(u)
 
     def unpack_x(self, x):
         beta = x[self.idx_beta]
@@ -127,3 +234,15 @@ class OptimizationSolver:
                              self.z*self.w.reshape(self.N, 1),
                              gamma,
                              self.n)
+
+
+def proj_capped_simplex(w, h):
+    a = np.min(w) - 1.0
+    b = np.max(w) - 0.0
+
+    def f(w_shift):
+        return np.sum(np.maximum(np.minimum(w - w_shift, 1.0), 0.0)) - h
+
+    w_shift_soln = bisect(f, a, b)
+
+    return np.maximum(np.minimum(w - w_shift_soln, 1.0), 0.0)
