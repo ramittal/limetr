update and fix: add the test for objective and gradient and fix small problems in the optim module

src/limetr/linalg.py
@@ -90,8 +90,7 @@ class VarMat:
         self.check_input()
 
         # convert to izmat format
-        self.scaled_z = ((self.re_mat*np.sqrt(self.gamma))/
-                         self.obs_sd.reshape(self.num_obs, 1))
+        self.scaled_z = ((self.re_mat*np.sqrt(self.gamma.real)).T/self.obs_sd).T
         # lsvd of izmat
         self.scaled_z_ns = np.minimum(self.group_sizes, self.dim_gamma)
         self.scaled_z_nu = self.group_sizes*self.scaled_z_ns
@@ -130,9 +129,9 @@ class VarMat:
         """
         naive implementation of the varmat
         """
-        split_idx = np.cumsum(self.group_sizes)[:-1]
-        obs_sd_list = np.split(self.obs_sd, split_idx)
-        re_mat_list = np.split(self.re_mat, split_idx, axis=0)
+        idx_split = np.cumsum(self.group_sizes)[:-1]
+        obs_sd_list = np.split(self.obs_sd, idx_split)
+        re_mat_list = np.split(self.re_mat, idx_split, axis=0)
 
         diag_blocks = [
             np.diag(obs_sd_list[i]**2) +
@@ -147,9 +146,9 @@ class VarMat:
         """
         naive implementation of the inverse varmat
         """
-        split_idx = np.cumsum(self.group_sizes)[:-1]
-        obs_sd_list = np.split(self.obs_sd, split_idx)
-        re_mat_list = np.split(self.re_mat, split_idx, axis=0)
+        idx_split = np.cumsum(self.group_sizes)[:-1]
+        obs_sd_list = np.split(self.obs_sd, idx_split)
+        re_mat_list = np.split(self.re_mat, idx_split, axis=0)
 
         diag_blocks = [
             np.linalg.inv(np.diag(obs_sd_list[i] ** 2) +

src/limetr/optim.py
@@ -17,7 +17,7 @@ class OptimizationSolver:
                  qm=None, qw=None,
                  cf=None, cl=None, cu=None,
                  pf=None, pm=None, pw=None,
-                 p=1.0):
+                 inlier_pct=1.0):
         # pass in data
         self.n = n
         self.y = y
@@ -52,7 +52,6 @@ class OptimizationSolver:
             self.cl = []
             self.cu = []
 
-
         self.pf = pf
         self.pm = pm
         self.pw = pw
@@ -79,9 +78,9 @@ class OptimizationSolver:
         self.N = sum(self.n)
 
         # trimming settings
-        self.h = int(p*self.N)
-        self.p = self.h/self.N
-        self.w = np.repeat(self.p, self.N)
+        self.h = int(inlier_pct*self.N)
+        self.inlier_pct = self.h/self.N
+        self.w = np.repeat(self.inlier_pct, self.N)
 
     def objective(self, x):
         beta, gamma = self.unpack_x(x)
@@ -103,14 +102,17 @@ class OptimizationSolver:
         beta, gamma = self.unpack_x(x)
         r = self.compute_r(beta)
         v = self.compute_v(gamma)
+        z = (self.z.T*np.sqrt(self.w)).T
+        jf = (self.f.jac(beta).T*np.sqrt(self.w)).T
+
         inv_v_r = v.inv_dot(r)
-        inv_v_z = v.inv_dot(self.z)
+        inv_v_z = v.inv_dot(z)
 
         # gradient for beta
-        g_beta = -self.f.jac(beta).T.dot(inv_v_r)/self.h
+        g_beta = -jf.T.dot(inv_v_r)/self.h
 
         # gradient for gamma
-        g_gamma = 0.5*(np.sum(self.z*inv_v_z, axis=0) -
+        g_gamma = 0.5*(np.sum(z*inv_v_z, axis=0) -
                        np.sum(np.add.reduceat(inv_v_z.T*r,
                                               self.idx_split,
                                               axis=1)**2, axis=1))/self.h
@@ -261,11 +263,11 @@ class OptimizationSolver:
         return beta, gamma
 
     def compute_r(self, beta):
-        return (self.y - self.f(beta))*self.w
+        return (self.y - self.f(beta))*np.sqrt(self.w)
 
     def compute_v(self, gamma):
         return linalg.VarMat(self.s**self.w,
-                             self.z*self.w.reshape(self.N, 1),
+                             (self.z.T*np.sqrt(self.w)).T,
                              gamma,
                              self.n)
 

tests/test_optim.py
@@ -9,6 +9,7 @@ import numpy as np
 import pytest
 import limetr.optim as optim
 import limetr.utils as utils
+import limetr.linalg as linalg
 
 
 @pytest.mark.parametrize(("w", "h", "tr_w"),
@@ -50,8 +51,73 @@ def opt_solver():
     pm = np.array([1.0]*pf.shape[0])
     pw = np.array([1.0]*pf.shape[0])
 
+    inlier_pct = 0.95
+
     return optim.OptimizationSolver(n, y, f, z, s=s,
                                     bl=bl, bu=bu,
                                     qm=qm, qw=qw,
                                     cf=cf, cl=cl, cu=cu,
-                                    pf=pf, pm=pm, pw=pw)
+                                    pf=pf, pm=pm, pw=pw,
+                                    inlier_pct=inlier_pct)
+
+
+def true_objective(x, opt_solver):
+    w = opt_solver.w
+    n = opt_solver.n
+    h = opt_solver.h
+    y = opt_solver.y
+    s = opt_solver.s
+    f = opt_solver.f
+    z = opt_solver.z
+    qw = opt_solver.qw
+    qm = opt_solver.qm
+    pf = opt_solver.pf
+    pw = opt_solver.pw
+    pm = opt_solver.pm
+
+    beta, gamma = opt_solver.unpack_x(x)
+    r = (y - f(beta)) * np.sqrt(w)
+    v = linalg.VarMat(s**w,
+                      (z.T*np.sqrt(w)).T,
+                      gamma,
+                      n).mat
+    inv_v = np.linalg.inv(v)
+
+    obj = 0.5*(r.dot(inv_v.dot(r)) + np.log(np.linalg.det(v)))/h
+    obj += 0.5*qw.dot((x - qm)**2)
+    obj += 0.5*pw.dot((pf(x) - pm)**2)
+
+    return obj
+
+
+def true_gradient(x, opt_solver, eps=1e-10):
+    xc = x + 0j
+    grad = np.zeros(x.size)
+    for i in range(x.size):
+        xc[i] += eps*1j
+        grad[i] = true_objective(xc, opt_solver).imag/eps
+        xc[i] -= eps*1j
+
+    return grad
+
+
+def test_optimization_solver_objective(opt_solver):
+    beta = np.random.randn(opt_solver.k_beta)
+    gamma = np.random.rand(opt_solver.k_gamma)
+    x = np.hstack((beta, gamma))
+
+    obj = true_objective(x, opt_solver)
+    my_obj = opt_solver.objective(x)
+
+    assert np.abs(my_obj - obj) < 1e-10
+
+
+def test_optimization_solver_gradient(opt_solver):
+    beta = np.random.randn(opt_solver.k_beta)
+    gamma = np.random.rand(opt_solver.k_gamma)
+    x = np.hstack((beta, gamma))
+
+    grad = true_gradient(x, opt_solver)
+    my_grad = opt_solver.gradient(x)
+
+    assert np.linalg.norm(my_grad - grad) < 1e-10